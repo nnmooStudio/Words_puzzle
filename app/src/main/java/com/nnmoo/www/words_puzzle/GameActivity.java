2c24cbbfe5f6a99aeae7260a81c60a57decc7784
Nnmoo.com <zhoufeng_dev@hotmail.com>
2016-05-30 22:03:28 +0800

    basic function

--- app/src/main/java/com/nnmoo/www/words_puzzle/GameActivity.java
+++ app/src/main/java/com/nnmoo/www/words_puzzle/GameActivity.java
@@ -1,13 +1,172 @@
 package com.nnmoo.www.words_puzzle;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GameActivity extends AppCompatActivity {
+    private int red_scores = 0;
+    private int blue_scores = 0;
+    private TextView textView_red;
+    private TextView textView_blue;
+    private ImageView imageView_next;
+    private ImageView imageView_red;
+    private ImageView imageView_blue;
+    private TextView textView_show_words;
+    MediaPlayer beep;
+    MediaPlayer drum_song;
+    MediaPlayer drum_end;
+    ImageView imageView_play;
+    Animation animation_alpha;
+    Animation animation_scale;
+    AnimationSet animation_set;
+    Animation animation_rotate;
+    Animation animation_alpha_play;
+    Timer timer;
+    Random random;
+    String[] words;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        //////////////////////////////////////////////////
+        //textView_show_words
+        textView_show_words = (TextView) findViewById(R.id.textView_show_words);
+        //beep声音
+        beep = MediaPlayer.create(this, R.raw.beep);
+        //drum声音设置
+        drum_song = MediaPlayer.create(this, R.raw.drum_song);
+        drum_end = MediaPlayer.create(this, R.raw.drum_for_end);
+        AnimationSetup();//动画设置
+        //next按钮
+        imageView_next = (ImageView) findViewById(R.id.imageView_next);
+        imageView_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView_next.startAnimation(animation_alpha);
+                beep.start();
+            }
+        });
+        //red按钮事件
+        imageView_red = (ImageView) findViewById(R.id.imageView_red);
+        imageView_red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView_red.startAnimation(animation_scale);
+                red_scores++;
+                textView_red.setText("红队："+red_scores + "分");
+                beep.start();
+            }
+        });
+        //blue按钮事件
+        imageView_blue = (ImageView) findViewById(R.id.imageView_blue);
+        imageView_blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView_blue.startAnimation(animation_scale);
+                blue_scores++;
+                textView_blue.setText("蓝队："+blue_scores + "分");
+                beep.start();
+            }
+        });
+        //play按钮事件
+        imageView_play = (ImageView) findViewById(R.id.imageView_play);
+        imageView_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                beep.start();
+                if (drum_song.isPlaying()) {
+                    drum_song.stop();
+                    imageView_play.clearAnimation();
+                    textView_show_words.setText("点播放按钮开始");
+                } else {
+                    reset_drum_song();
+                    imageView_play.startAnimation(animation_set);
+                    drum_song.start();
+                    textView_show_words.setText("点下一个按钮");
+                    start_game();
+                }
+            }
+        });
+        //textView_red/blue事件
+        textView_red = (TextView) findViewById(R.id.textView_red);
+        textView_blue = (TextView) findViewById(R.id.textView_blue);
     }
-}
+        //重置声音资源
+    private void reset_drum_song() {
+        drum_song = MediaPlayer.create(this, R.raw.drum_song);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        stop_Animation_music();
+    }
+
+    //页面回来事件
+    protected void onRestart() {
+        super.onRestart();
+        reset_drum_song();
+    }
+
+    //动画设置
+    private void AnimationSetup() {
+        animation_alpha = new AlphaAnimation(1.0F, 0.5F);
+        animation_alpha.setDuration(200);
+        animation_scale = new ScaleAnimation(1.0F, 0.95F, 1.0F, 0.95F);
+        animation_scale.setDuration(200);
+        animation_rotate = new RotateAnimation(0f, 360f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        animation_alpha_play = new AlphaAnimation(1.0F, 0.5F);
+        animation_rotate.setDuration(1000);
+        animation_alpha_play.setDuration(1000);
+        animation_rotate.setRepeatCount(Animation.INFINITE);
+        animation_alpha_play.setRepeatCount(Animation.INFINITE);
+        animation_set = new AnimationSet(false);
+        animation_set.addAnimation(animation_rotate);
+        animation_set.addAnimation(animation_alpha_play);
+    }
+
+    //start_game_event
+    private void start_game() {
+        random = new Random(60);
+        timer = new Timer();
+        long time_to_stop = random.nextInt() * 1000 + 45000;
+        timer.schedule(new game_time_event(), 5000L, 9000L);
+    }
+
+    //stop_Animation
+    private void stop_Animation_music() {
+        drum_song.stop();
+        imageView_play.clearAnimation();
+    }
+
+
+
+
+
+    class game_time_event extends TimerTask {
+        public void run() {
+            //drum_song.stop();
+
+            drum_end.start();
+            timer.cancel();
+            drum_song.stop();
+            imageView_play.clearAnimation();
+            //imageView_play.callOnClick();
+        }
+    }
+    }
+
--- app/src/main/java/com/nnmoo/www/words_puzzle/MainActivity.java
+++ app/src/main/java/com/nnmoo/www/words_puzzle/MainActivity.java
@@ -1,13 +1,86 @@
 package com.nnmoo.www.words_puzzle;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView_start_game;
+    ImageView imageView_logo;
+    TextView textView_kuaicai;
+    MediaPlayer beep;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //beep声音
+        beep=MediaPlayer.create(this,R.raw.beep);
+
+        //开始游戏按钮事件
+        imageView_start_game=(ImageView) findViewById(R.id.imageView_start_game);
+        imageView_start_game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation animation_alpha=new AlphaAnimation(1.0f,0.0f);
+                animation_alpha.setDuration(300);
+                imageView_start_game.startAnimation(animation_alpha);
+                beep.start();
+                Intent intent_sart_game=new Intent("com.nnmoo.www.words_puzzle.GameActivity");
+                startActivity(intent_sart_game);
+            }
+        });
+        //词logo动画事件
+        imageView_logo=(ImageView)findViewById(R.id.imageView_words_logo);
+        final Animation animation_alpha=new AlphaAnimation(1.0F,0.0F);
+        final Animation animation_alpha2=new AlphaAnimation(0.0F,1.0F);
+        imageView_logo.startAnimation(animation_alpha);
+        animation_alpha.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView_logo.startAnimation(animation_alpha2);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        animation_alpha2.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView_logo.startAnimation(animation_alpha);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        // imageView_logo.startAnimation(animation_alpha2);
+        animation_alpha.setDuration(5000);
+        //animation_alpha.setRepeatCount(Animation.INFINITE);
+        animation_alpha2.setDuration(5000);
+        // animation_alpha2.setRepeatCount(Animation.INFINITE);
+        // animation_alpha2.setStartOffset(5000);
+
+
     }
 }
